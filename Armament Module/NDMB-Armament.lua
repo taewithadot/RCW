331e0ec6086f38265924e62caca9540b41540f26
taewithadot <churpyurpy@gmail.com>
2022-05-07 04:20:43 +0100

    Armament Module Initial Version

--- Armament Module/NDMB-Armament.lua
+++ Armament Module/NDMB-Armament.lua
@@ -1,49 +1,149 @@
 --Neko PMC Armament Module
 
+trigger.action.outText("NDMB Armament Module v1.00 loaded!", 10) --DEBUG
+
+UnitPunishmentImminent = {}
+
+function IntegratedbasicSerialize(s)
+    if s == nil then
+      return "\"\""
+    else
+      if ((type(s) == 'number') or (type(s) == 'boolean') or (type(s) == 'function') or (type(s) == 'table') or (type(s) == 'userdata') ) then
+        return tostring(s)
+      elseif type(s) == 'string' then
+        return string.format('%q', s)
+      end
+    end
+end
+
+
+function IntegratedserializeWithCycles(name, value, saved)
+  local basicSerialize = function (o)
+    if type(o) == "number" then
+      return tostring(o)
+    elseif type(o) == "boolean" then
+      return tostring(o)
+    else -- assume it is a string
+      return IntegratedbasicSerialize(o)
+    end
+  end
+
+  local t_str = {}
+  saved = saved or {}       -- initial value
+  if ((type(value) == 'string') or (type(value) == 'number') or (type(value) == 'table') or (type(value) == 'boolean')) then
+    table.insert(t_str, name .. " = ")
+    if type(value) == "number" or type(value) == "string" or type(value) == "boolean" then
+      table.insert(t_str, basicSerialize(value) ..  "\n")
+    else
+
+      if saved[value] then    -- value already saved?
+        table.insert(t_str, saved[value] .. "\n")
+      else
+        saved[value] = name   -- save name for next time
+        table.insert(t_str, "{}\n")
+        for k,v in pairs(value) do      -- save its fields
+          local fieldname = string.format("%s[%s]", name, basicSerialize(k))
+          table.insert(t_str, IntegratedserializeWithCycles(fieldname, v, saved))
+        end
+      end
+    end
+    return table.concat(t_str)
+  else
+    return ""
+  end
+end
+
+
 function writetofile(data, file)--Function for saving to file (commonly found)
 	File = io.open(file, "w")
 	File:write(data)
 	File:close()
 end
 
+function punishPlayer(unitName, time)
+
+	trigger.action.outText("AW JESUS!", 10) --DEBUG
+	if UnitPunishmentImminent[unitName] == true then
+		trigger.action.setUnitInternalCargo(unitName, 1000000)
+	end
+
+end
 
 LoadoutCheck_Takeoff = {}
 function LoadoutCheck_Takeoff:onEvent(Event)
 
-	if Event.id == world.event.S_EVENT_TAKEOFF and Event.initiator and Event.initiator.getPlayerName() then --If initiator exists and they are a player
+	if Event.id ~= world.event.S_EVENT_TAKEOFF then
+		return
+	end
 
-		--insert code to check what wing they are part of here
-		--gonna need to check MP player name against a list of MP player names and their wing as a value (0=NRF, 1=first wing, etc)
-		dofile("NekoWingMembers_static.lua")
-		--insert stockpile loading based on wing membership
-		local ammo = Event.initiator:getAmmo()
+	if Event.initiator and Event.initiator:getPlayerName() then --If initiator exists and they are a player
+
+		--dofile("wingmembers_default.lua") --load wingmember list
+		--local wing = wingMember[Event.initiator:getPlayerName()] --set wing to players wing - usable when wingmember lua gets loaded
+
+		trigger.action.outText("Player " .. " took off!", 10) --DEBUG
+
+		UnitPunishmentImminent[Event.initiator:getName()] = false --reset punishment flag
+
+		local ammo = Event.initiator:getAmmo() --get loadout of player
+
+		dofile("stockpile_static.lua") --get current stockpile
+
+		--Check for valid munitions
 
 		for i = 1, #ammo do
 
 			local weaponType = ammo[i].desc.typeName
 			local weaponCount = ammo[i].count
 
-			if stockpile_static[weaponType] < weaponCount then
-				--punish player
-			elseif 
-				--subtract weapon from stockpile, dont know if below works?
-				stockpile_static[weaponType] = stockpile_static[weaponType] - weaponCount
+			if stockpile_static[weaponType] ~= nil and stockpile_static[weaponType] < weaponCount then
+				UnitPunishmentImminent[Event.initiator:getName()] = true -- flag user for punishment
+				trigger.action.outText("INVALID PYLON DETECTED FOR " .. Event.initiator:getPlayerName() .. " !", 10) --DEBUG
 			end
+
 		end
+
+		if UnitPunishmentImminent[Event.initiator:getName()] == true then
+			timer.scheduleFunction(punishPlayer, Event.initiator:getName(), timer.getTime() + 180) --schedule punishPlayer function
+			trigger.action.outText("Player " .. Event.initiator:getPlayerName() .. " took off with an invalid loadout! Land within three minutes or face the wrath of the gods!", 10) --DEBUG
+			return
+		end
+
+		
+
+		for i = 1, #ammo do
+
+			local weaponType = ammo[i].desc.typeName
+			local weaponCount = ammo[i].count
+
+			if stockpile_static[weaponType] ~= nil and stockpile_static[weaponType] >= weaponCount then
+				stockpile_static[weaponType] = stockpile_static[weaponType] - weaponCount --subtract weapon from stockpile
+				trigger.action.outText("Subtracting from stockpile..", 10) --DEBUG
+
+			end
+
+		end
+
+		stockpile_serialised = IntegratedserializeWithCycles("stockpile_static", stockpile_static)
+		writetofile(stockpile_serialised, "stockpile_static.lua")
+			
 	end 
 end
 world.addEventHandler(LoadoutCheck_Takeoff)
 
 
-LoadoutCheck_Landing = {}
-function LoadoutCheck_Landing:onEvent(Event)
 
-	if Event.id == world.event.S_EVENT_LANDING and Event.initiator and Event.initiator.getPlayerName() then --If initiator exists and they are a player
 
-		--insert code to check what wing they are part of here
-		--gonna need to check MP player name against a list of MP player names and their wing as a value (0=NRF, 1=first wing, etc)
-		dofile("NekoWingMembers_static.lua")
-		--insert stockpile loading based on wing membership here
+
+LoadoutCheck_Land = {}
+function LoadoutCheck_Land:onEvent(Event)
+
+	if Event.id ~= world.event.S_EVENT_LAND then
+		return
+	end
+
+	if Event.initiator and Event.initiator:getPlayerName() and UnitPunishmentImminent[Event.initiator:getName()] == false then --If initiator exists and they are a player
+
 		local ammo = Event.initiator:getAmmo()
 
 		for i = 1, #ammo do
@@ -51,10 +151,21 @@ function LoadoutCheck_Landing:onEvent(Event)
 			local weaponType = ammo[i].desc.typeName
 			local weaponCount = ammo[i].count
 
-			stockpile_static[WeaponType] = stockpile_static[weaponType] + weaponCount
+			if stockpile_static[weaponType] ~= nil then
+				stockpile_static[weaponType] = stockpile_static[weaponType] + weaponCount
+				trigger.action.outText("Returning weapon to stockpile..", 10) --DEBUG
+			end
 
 		end
 
-	end
-world.addEventHandler(LoadoutCheck_Landing)
+	stockpile_serialised = IntegratedserializeWithCycles("stockpile_static", stockpile_static)
+	writetofile(stockpile_serialised, "stockpile_static.lua")
 
+
+	elseif UnitPunishmentImminent[Event.initiator:getName()] == true then
+		trigger.action.outText(Event.initiator:getPlayerName() .. " has found salvation!", 10) --DEBUG
+		UnitPunishmentImminent[Event.initiator:getName()] = false
+	end
+
+end
+world.addEventHandler(LoadoutCheck_Land)
--- Armament Module/data/stockpile_default.lua
+++ Armament Module/data/stockpile_default.lua
@@ -1,196 +1,196 @@
---stockpile defaults
+--stockpile_static defaults
 
 --NOTE - THIS FILE IS STILL UNDER CONSTRUCTION! NOT ALL WEAPON TYPES ARE PRESENT!
 
-stockpile = {}
+stockpile_static = {}
 
 ----Western Air to Air----
 --Murica
-stockpile["weapons.missiles.AIM_120C"] = 999
-stockpile["AIM_9"] = 999
-stockpile["weapons.missiles.AIM_120"] = 999
-stockpile["AIM_9X"] = 999
-stockpile["AIM-9L"] = 999
-stockpile["AIM-9P"] = 999
-stockpile["GAR-8"] = 999 --AIM-9B
-stockpile["weapons.missiles.AIM-7MH"] = 999
-stockpile["weapons.missiles.AIM-7F"] = 999
-stockpile["weapons.missiles.AIM_7"] = 999
+stockpile_static["weapons.missiles.AIM_120C"] = 999
+stockpile_static["AIM_9"] = 999
+stockpile_static["weapons.missiles.AIM_120"] = 999
+stockpile_static["AIM_9X"] = 999
+stockpile_static["AIM-9L"] = 999
+stockpile_static["AIM-9P"] = 999
+stockpile_static["GAR-8"] = 999 --AIM-9B
+stockpile_static["weapons.missiles.AIM-7MH"] = 999
+stockpile_static["weapons.missiles.AIM-7F"] = 999
+stockpile_static["weapons.missiles.AIM_7"] = 999
 --M2K
-stockpile["MMagicII"] = 999
-stockpile["weapons.missiles.Matra Super 530D"] = 999
+stockpile_static["MMagicII"] = 999
+stockpile_static["weapons.missiles.Matra Super 530D"] = 999
 --F-14
-stockpile["AIM_54A_Mk47"] = 999
-stockpile["AIM_54A_Mk60"] = 999
-stockpile["AIM_54C_Mk47"] = 999
+stockpile_static["AIM_54A_Mk47"] = 999
+stockpile_static["AIM_54A_Mk60"] = 999
+stockpile_static["AIM_54C_Mk47"] = 999
 --Viggen
-stockpile["Rb 24J"] = 999
-stockpile["Rb 24"] = 999
-stockpile["Rb 74"] = 999
+stockpile_static["Rb 24J"] = 999
+stockpile_static["Rb 24"] = 999
+stockpile_static["Rb 74"] = 999
 --m'nem jeff
-stockpile["PL-5EII"] = 999
-stockpile["SD-10"] = 999
+stockpile_static["PL-5EII"] = 999
+stockpile_static["SD-10"] = 999
 
 ----Russian Air to Air----
 --MiG21
-stockpile["R-3S"] = 999
-stockpile["R-3R"] = 999
-stockpile["R-13M"] = 999
-stockpile["R-55"] = 999
-stockpile["R-13M1"] = 999
-stockpile["RS2US"] = 999
-stockpile["R-60"] = 999
-stockpile["P_60"] = 999 --R-60M
+stockpile_static["R-3S"] = 999
+stockpile_static["R-3R"] = 999
+stockpile_static["R-13M"] = 999
+stockpile_static["R-55"] = 999
+stockpile_static["R-13M1"] = 999
+stockpile_static["RS2US"] = 999
+stockpile_static["R-60"] = 999
+stockpile_static["P_60"] = 999 --R-60M
 --Mig29/Su27/Su33/J-11
-stockpile["P_73"] = 999
-stockpile["P_27T"] = 999
-stockpile["P_27TE"] = 999
-stockpile["P_27P"] = 999
-stockpile["P_27PE"] = 999
-stockpile["P_77"] = 999
+stockpile_static["P_73"] = 999
+stockpile_static["P_27T"] = 999
+stockpile_static["P_27TE"] = 999
+stockpile_static["P_27P"] = 999
+stockpile_static["P_27PE"] = 999
+stockpile_static["P_77"] = 999
 
 
 
 --Western Air to Ground
 --MURICA!
-stockpile["weapons.bombs.Mk_81"] = 999
-stockpile["weapons.bombs.Mk_82"] = 999
-stockpile["weapons.bombs.MK_82AIR"] = 999
-stockpile["weapons.bombs.MK_82SNAKEYE"] = 999
-stockpile["weapons.bombs.Mk_83"] = 999
-stockpile["weapons.bombs.Mk_84"] = 999
-stockpile["weapons.bombs.GBU_10"] = 999
-stockpile["weapons.bombs.GBU_12"] = 999
-stockpile["weapons.bombs.GBU_16"] = 999
-stockpile["weapons.bombs.GBU_24"] = 999
-stockpile["weapons.bombs.GBU_31"] = 999
-stockpile["weapons.bombs.GBU_31_V_3B"] = 999
-stockpile["weapons.bombs.GBU_32_V_2B"] = 999
-stockpile["weapons.bombs.GBU_38"] = 999
-stockpile["weapons.bombs.GBU_54_V_1B"] = 999
-stockpile["weapons.bombs.CBU_87"] = 999
-stockpile["weapons.bombs.CBU_97"] = 999
-stockpile["weapons.bombs.CBU_99"] = 999
-stockpile["weapons.bombs.CBU_103"] = 999
-stockpile["weapons.bombs.CBU_105"] = 999
-stockpile["weapons.bombs.ROCKEYE"] = 999
+stockpile_static["weapons.bombs.Mk_81"] = 999
+stockpile_static["weapons.bombs.Mk_82"] = 999
+stockpile_static["weapons.bombs.MK_82AIR"] = 999
+stockpile_static["weapons.bombs.MK_82SNAKEYE"] = 999
+stockpile_static["weapons.bombs.Mk_83"] = 999
+stockpile_static["weapons.bombs.Mk_84"] = 999
+stockpile_static["weapons.bombs.GBU_10"] = 999
+stockpile_static["weapons.bombs.GBU_12"] = 999
+stockpile_static["weapons.bombs.GBU_16"] = 999
+stockpile_static["weapons.bombs.GBU_24"] = 999
+stockpile_static["weapons.bombs.GBU_31"] = 999
+stockpile_static["weapons.bombs.GBU_31_V_3B"] = 999
+stockpile_static["weapons.bombs.GBU_32_V_2B"] = 999
+stockpile_static["weapons.bombs.GBU_38"] = 999
+stockpile_static["weapons.bombs.GBU_54_V_1B"] = 999
+stockpile_static["weapons.bombs.CBU_87"] = 999
+stockpile_static["weapons.bombs.CBU_97"] = 999
+stockpile_static["weapons.bombs.CBU_99"] = 999
+stockpile_static["weapons.bombs.CBU_103"] = 999
+stockpile_static["weapons.bombs.CBU_105"] = 999
+stockpile_static["weapons.bombs.ROCKEYE"] = 999
 
 
-stockpile["weapons.missiles.AGM_65D"] = 999
-stockpile["weapons.missiles.AGM_65E"] = 999
-stockpile["weapons.missiles.AGM_65F"] = 999
-stockpile["weapons.missiles.AGM_65G"] = 999
-stockpile["weapons.missiles.AGM_65H"] = 999
-stockpile["weapons.missiles.AGM_65K"] = 999
-stockpile["weapons.missiles.AGM_65L"] = 999
-stockpile["weapons.missiles.AGM_84D"] = 999
-stockpile["weapons.missiles.AGM_84H"] = 999
-stockpile["weapons.missiles.AGM_84E"] = 999
-stockpile["weapons.missiles.AGM_88"] = 999
-Armour["weapons.missiles.AGM_114K"] = 999 --Hellfire
-stockpile["weapons.missiles.AGM_122"] = 999 --sidearm
-stockpile["weapons.missiles.AGM_154A"] = 999
-stockpile["weapons.missiles.AGM_154C"] = 999
-stockpile["weapons.missiles.ADM_141A"] = 999 --F-14 TALD
+stockpile_static["weapons.missiles.AGM_65D"] = 999
+stockpile_static["weapons.missiles.AGM_65E"] = 999
+stockpile_static["weapons.missiles.AGM_65F"] = 999
+stockpile_static["weapons.missiles.AGM_65G"] = 999
+stockpile_static["weapons.missiles.AGM_65H"] = 999
+stockpile_static["weapons.missiles.AGM_65K"] = 999
+stockpile_static["weapons.missiles.AGM_65L"] = 999
+stockpile_static["weapons.missiles.AGM_84D"] = 999
+stockpile_static["weapons.missiles.AGM_84H"] = 999
+stockpile_static["weapons.missiles.AGM_84E"] = 999
+stockpile_static["weapons.missiles.AGM_88"] = 999
+stockpile_static["weapons.missiles.AGM_114K"] = 999 --Hellfire
+stockpile_static["weapons.missiles.AGM_122"] = 999 --sidearm
+stockpile_static["weapons.missiles.AGM_154A"] = 999
+stockpile_static["weapons.missiles.AGM_154C"] = 999
+stockpile_static["weapons.missiles.ADM_141A"] = 999 --F-14 TALD
 
 
-stockpile["weapons.missiles.AGR_20A"] = 999 -- HE APKWS
-stockpile["weapons.missiles.AGR_20_M282"] = 999 --MPP APKWS
-stockpile["weapons.nurs.HYDRA_70_MK5"] = 999 --HEAT Rockets
-stockpile["weapons.nurs.HYDRA_70_M151"] = 999 --HE Rockets
-stockpile["weapons.nurs.HYDRA_70_M156"] = 999 --White Phosphorus Rockets
-stockpile["weapons.nurs.HYDRA_70_M282"] = 999 -- APACHE MPP
-stockpile["weapons.nurs.HYDRA_70_M229"] = 999 -- APACHE HEDP
+stockpile_static["weapons.missiles.AGR_20A"] = 999 -- HE APKWS
+stockpile_static["weapons.missiles.AGR_20_M282"] = 999 --MPP APKWS
+stockpile_static["weapons.nurs.HYDRA_70_MK5"] = 999 --HEAT Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M151"] = 999 --HE Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M156"] = 999 --White Phosphorus Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M282"] = 999 -- APACHE MPP
+stockpile_static["weapons.nurs.HYDRA_70_M229"] = 999 -- APACHE HEDP
 
-stockpile["weapons.nurs.Zuni_127"] = 999
+stockpile_static["weapons.nurs.Zuni_127"] = 999
 
 --Viggen..
-stockpile["Rb 04E"] = 999
-stockpile["weapons.missiles.BK90_MJ1"] = 999 --cluster bomblets
-stockpile["weapons.missiles.BK90_MJ2"] = 999 --cluster bomblets
-stockpile["weapons.missiles.BK90_MJ1_MJ2"] = 999 --cluster bomblets
-stockpile["weapons.missiles.RB75"] = 999 --basically mav
-stockpile["weapons.missiles.RB75B"] = 999 --basically mav
-stockpile["weapons.missiles.RB75T"] = 999 --basically mav
-stockpile["Rb 15F"] = 999 --chonkers ASM
-stockpile["Rb 05A"] = 999
-stockpile["weapons.bombs.HEBOMB"] = 999
-stockpile["weapons.bombs.HEBOMBD"] = 999
+stockpile_static["Rb 04E"] = 999
+stockpile_static["weapons.missiles.BK90_MJ1"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.BK90_MJ2"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.BK90_MJ1_MJ2"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.RB75"] = 999 --basically mav
+stockpile_static["weapons.missiles.RB75B"] = 999 --basically mav
+stockpile_static["weapons.missiles.RB75T"] = 999 --basically mav
+stockpile_static["Rb 15F"] = 999 --chonkers ASM
+stockpile_static["Rb 05A"] = 999
+stockpile_static["weapons.bombs.HEBOMB"] = 999
+stockpile_static["weapons.bombs.HEBOMBD"] = 999
 
 --mNEM JEEEFFF
-stockpile["weapons.missiles.GB-6"] = 999
-stockpile["weapons.missiles.GB-6-HE"] = 999
-stockpile["weapons.missiles.GB-6-SFW"] = 999
-stockpile["weapons.nurs.UG_90MM"] = 999
-stockpile["weapons.missiles.BRM-1_90MM"] = 999
+stockpile_static["weapons.missiles.GB-6"] = 999
+stockpile_static["weapons.missiles.GB-6-HE"] = 999
+stockpile_static["weapons.missiles.GB-6-SFW"] = 999
+stockpile_static["weapons.nurs.UG_90MM"] = 999
+stockpile_static["weapons.missiles.BRM-1_90MM"] = 999
 
-stockpile["weapons.missiles.C-701T"] = 999
-stockpile["weapons.missiles.C-701IR"] = 999
-stockpile["CM-802AKG"] = 999
-stockpile["weapons.missiles.C-802AK"] = 999
-stockpile["weapons.missiles.LD-10"] = 999
-stockpile["weapons.bombs.TYPE-200A"] = 999
-stockpile["weapons.missiles.LS-6-500"] = 999
+stockpile_static["weapons.missiles.C-701T"] = 999
+stockpile_static["weapons.missiles.C-701IR"] = 999
+stockpile_static["CM-802AKG"] = 999
+stockpile_static["weapons.missiles.C-802AK"] = 999
+stockpile_static["weapons.missiles.LD-10"] = 999
+stockpile_static["weapons.bombs.TYPE-200A"] = 999
+stockpile_static["weapons.missiles.LS-6-500"] = 999
 
 --Baguette HON HON
-stockpile["weapons.bombs.BAP_100"] = 999
-stockpile["weapons.bombs.BLG66_BELOUGA"] = 999
+stockpile_static["weapons.bombs.BAP_100"] = 999
+stockpile_static["weapons.bombs.BLG66_BELOUGA"] = 999
 
 
 --Russian Air to Ground
 
-stockpile["weapons.bombs.FAB_250"] = 999
-stockpile["weapons.bombs.FAB-250-M62"] = 999
-stockpile["weapons.bombs.FAB_500"] = 999
-stockpile["weapons.bombs.RBK_250"] = 999
-stockpile["weapons.bombs.RBK_250_275_AO_1SCH"] = 999 --RBK-250-275
-stockpile["weapons.bombs.RBK_500AO"] = 999 --RBK-500-255
-stockpile["weapons.bombs.RBK_500U"] = 999 --RBK 500
-stockpile["weapons.bombs.RBK_500U_OAB_2_SRT"] = 999 --RBK 500U
-stockpile["weapons.bombs.BetAB_500"] = 999
-stockpile["weapons.bombs.BetAB_500ShP"] = 999
-stockpile["weapons.bombs.BKF_AO2_5RT"] = 999 --HE/FRAG bomblet dispenser
-stockpile["weapons.bombs.BKF_PTAB_5KO"] = 999 --HEAT bomblet dispenser
+stockpile_static["weapons.bombs.FAB_250"] = 999
+stockpile_static["weapons.bombs.FAB-250-M62"] = 999
+stockpile_static["weapons.bombs.FAB_500"] = 999
+stockpile_static["weapons.bombs.RBK_250"] = 999
+stockpile_static["weapons.bombs.RBK_250_275_AO_1SCH"] = 999 --RBK-250-275
+stockpile_static["weapons.bombs.RBK_500AO"] = 999 --RBK-500-255
+stockpile_static["weapons.bombs.RBK_500U"] = 999 --RBK 500
+stockpile_static["weapons.bombs.RBK_500U_OAB_2_SRT"] = 999 --RBK 500U
+stockpile_static["weapons.bombs.BetAB_500"] = 999
+stockpile_static["weapons.bombs.BetAB_500ShP"] = 999
+stockpile_static["weapons.bombs.BKF_AO2_5RT"] = 999 --HE/FRAG bomblet dispenser
+stockpile_static["weapons.bombs.BKF_PTAB_5KO"] = 999 --HEAT bomblet dispenser
 
 
-stockpile["weapons.nurs.C_5"] = 999 --S-5
-stockpile["weapons.nurs.C_8"] = 999 --S-8 HEAT
-stockpile["weapons.nurs.C_8OFP2"] = 999 --S-8 HE/FRAG
-stockpile["weapons.nurs.C_13"] = 999 --S-13
-stockpile["weapons.nurs.C_24"] = 999 --S-24B
-stockpile["weapons.nurs.C_25"] = 999 --S-25-OFM
-stockpile["weapons.nurs.S-25-O"] = 999 --S-25-O
+stockpile_static["weapons.nurs.C_5"] = 999 --S-5
+stockpile_static["weapons.nurs.C_8"] = 999 --S-8 HEAT
+stockpile_static["weapons.nurs.C_8OFP2"] = 999 --S-8 HE/FRAG
+stockpile_static["weapons.nurs.C_13"] = 999 --S-13
+stockpile_static["weapons.nurs.C_24"] = 999 --S-24B
+stockpile_static["weapons.nurs.C_25"] = 999 --S-25-OFM
+stockpile_static["weapons.nurs.S-25-O"] = 999 --S-25-O
 
 
 --mig21
-stockpile["weapons.nurs.S-24A"] = 999 --S-24A special mig version that slaps
-stockpile["weapons.nurs.S-24B"] = 999 --S-24B special mig version that slaps
-stockpile["weapons.bombs.RN-24"] = 999 --Small nuke
-stockpile["weapons.bombs.RN-28"] = 999 -- Big nuke
-stockpile["Kh-66_Grom"] = 999 --Air to ground missile, viability unknown
-stockpile["weapons.bombs.BL_755"] = 999 --some cluster bombs or something idk
-stockpile["weapons.bombs.FAB250M54TU"] = 999 --250kg bomb with drag chute
+stockpile_static["weapons.nurs.S-24A"] = 999 --S-24A special mig version that slaps
+stockpile_static["weapons.nurs.S-24B"] = 999 --S-24B special mig version that slaps
+stockpile_static["weapons.bombs.RN-24"] = 999 --Small nuke
+stockpile_static["weapons.bombs.RN-28"] = 999 -- Big nuke
+stockpile_static["Kh-66_Grom"] = 999 --Air to ground missile, viability unknown
+stockpile_static["weapons.bombs.BL_755"] = 999 --some cluster bombs or something idk
+stockpile_static["weapons.bombs.FAB250M54TU"] = 999 --250kg bomb with drag chute
 
 --froggy
-stockpile["S_25L"] = 999 -- Laser Guided huge ass rocket
-stockpile["weapons.bombs.KAB_500Kr"] = 999 -- TV Guided Bomb
-stockpile["weapons.missiles.Vikhr_M"] = 999 --Vikhr (ATGM)
-stockpile["weapons.missiles.X_25ML"] = 999 --Kh-25ML (Soy Laser Guided)
-stockpile["weapons.missiles.Kh25MP_PRGS1VP"] = 999 --Kh-25MP (Super-Soy HARM)
-stockpile["weapons.missiles.X_25MP"] = 999 --Kh-25MPU (Soy HARM)
-stockpile["weapons.missiles.X_29T"] = 999 --Kh-29T (Chad TV Guided)
-stockpile["weapons.missiles.X_29L"] = 999 --Kh-29L (Chad Laser Guided)
-stockpile["weapons.missiles.X_58"] = 999 --Kh-58 (Chad HARM)
+stockpile_static["S_25L"] = 999 -- Laser Guided huge ass rocket
+stockpile_static["weapons.bombs.KAB_500Kr"] = 999 -- TV Guided Bomb
+stockpile_static["weapons.missiles.Vikhr_M"] = 999 --Vikhr (ATGM)
+stockpile_static["weapons.missiles.X_25ML"] = 999 --Kh-25ML (Soy Laser Guided)
+stockpile_static["weapons.missiles.Kh25MP_PRGS1VP"] = 999 --Kh-25MP (Super-Soy HARM)
+stockpile_static["weapons.missiles.X_25MP"] = 999 --Kh-25MPU (Soy HARM)
+stockpile_static["weapons.missiles.X_29T"] = 999 --Kh-29T (Chad TV Guided)
+stockpile_static["weapons.missiles.X_29L"] = 999 --Kh-29L (Chad Laser Guided)
+stockpile_static["weapons.missiles.X_58"] = 999 --Kh-58 (Chad HARM)
 
 --hind
-stockpile["weapons.missiles.AT_6"] = 999 --Shturm ATGM
-stockpile["weapons.missiles.Ataka_9M120"] = 999 --Ataka HEAT ATGM
-stockpile["weapons.missiles.Ataka_9M120F"] = 999 --Ataka HE ATGM
-stockpile["weapons.missiles.Ataka_9M220"] = 999 -- Ataka AA Frag ATGM
+stockpile_static["weapons.missiles.AT_6"] = 999 --Shturm ATGM
+stockpile_static["weapons.missiles.Ataka_9M120"] = 999 --Ataka HEAT ATGM
+stockpile_static["weapons.missiles.Ataka_9M120F"] = 999 --Ataka HE ATGM
+stockpile_static["weapons.missiles.Ataka_9M220"] = 999 -- Ataka AA Frag ATGM
 
 --gazelle
-stockpile["weapons.nurs.SNEB68_EAP"] = 999 --gazelle rockets, why even
-stockpile["HOT3"] = 999 --ATGM
-stockpile["weapons.missiles.MBDA_Mistral"] = 999 -- AA Missile
+stockpile_static["weapons.nurs.SNEB68_EAP"] = 999 --gazelle rockets, why even
+stockpile_static["HOT3"] = 999 --ATGM
+stockpile_static["weapons.missiles.MBDA_Mistral"] = 999 -- AA Missile
 
-stockpile[""] = 999
+stockpile_static[""] = 999
