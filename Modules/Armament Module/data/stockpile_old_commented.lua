872a11c51e2c8cadf420dde76ca25b9ce7d756e6
taewithadot <churpyurpy@gmail.com>
2022-05-13 21:44:48 +0100

    Create stockpile_old_commented.lua

---
+++ Modules/Armament Module/data/stockpile_old_commented.lua
@@ -0,0 +1,196 @@
+--stockpile_static defaults
+
+-- this table is in the old serialise format and should be used as more of a reference
+
+stockpile_static = {}
+
+----Western Air to Air----
+--Murica
+stockpile_static["weapons.missiles.AIM_120C"] = 999
+stockpile_static["AIM_9"] = 999
+stockpile_static["weapons.missiles.AIM_120"] = 999
+stockpile_static["AIM_9X"] = 999
+stockpile_static["AIM-9L"] = 999
+stockpile_static["AIM-9P"] = 999
+stockpile_static["GAR-8"] = 999 --AIM-9B
+stockpile_static["weapons.missiles.AIM-7MH"] = 999
+stockpile_static["weapons.missiles.AIM-7F"] = 999
+stockpile_static["weapons.missiles.AIM_7"] = 999
+--M2K
+stockpile_static["MMagicII"] = 999
+stockpile_static["weapons.missiles.Matra Super 530D"] = 999
+--F-14
+stockpile_static["AIM_54A_Mk47"] = 999
+stockpile_static["AIM_54A_Mk60"] = 999
+stockpile_static["AIM_54C_Mk47"] = 999
+--Viggen
+stockpile_static["Rb 24J"] = 999
+stockpile_static["Rb 24"] = 999
+stockpile_static["Rb 74"] = 999
+--m'nem jeff
+stockpile_static["PL-5EII"] = 999
+stockpile_static["SD-10"] = 999
+
+----Russian Air to Air----
+--MiG21
+stockpile_static["R-3S"] = 999
+stockpile_static["R-3R"] = 999
+stockpile_static["R-13M"] = 999
+stockpile_static["R-55"] = 999
+stockpile_static["R-13M1"] = 999
+stockpile_static["RS2US"] = 999
+stockpile_static["R-60"] = 999
+stockpile_static["P_60"] = 999 --R-60M
+--Mig29/Su27/Su33/J-11
+stockpile_static["P_73"] = 999
+stockpile_static["P_27T"] = 999
+stockpile_static["P_27TE"] = 999
+stockpile_static["P_27P"] = 999
+stockpile_static["P_27PE"] = 999
+stockpile_static["P_77"] = 999
+
+
+
+--Western Air to Ground
+--MURICA!
+stockpile_static["weapons.bombs.Mk_81"] = 999
+stockpile_static["weapons.bombs.Mk_82"] = 999
+stockpile_static["weapons.bombs.MK_82AIR"] = 999
+stockpile_static["weapons.bombs.MK_82SNAKEYE"] = 999
+stockpile_static["weapons.bombs.Mk_83"] = 999
+stockpile_static["weapons.bombs.Mk_84"] = 999
+stockpile_static["weapons.bombs.GBU_10"] = 999
+stockpile_static["weapons.bombs.GBU_12"] = 999
+stockpile_static["weapons.bombs.GBU_16"] = 999
+stockpile_static["weapons.bombs.GBU_24"] = 999
+stockpile_static["weapons.bombs.GBU_31"] = 999
+stockpile_static["weapons.bombs.GBU_31_V_3B"] = 999
+stockpile_static["weapons.bombs.GBU_32_V_2B"] = 999
+stockpile_static["weapons.bombs.GBU_38"] = 999
+stockpile_static["weapons.bombs.GBU_54_V_1B"] = 999
+stockpile_static["weapons.bombs.CBU_87"] = 999
+stockpile_static["weapons.bombs.CBU_97"] = 999
+stockpile_static["weapons.bombs.CBU_99"] = 999
+stockpile_static["weapons.bombs.CBU_103"] = 999
+stockpile_static["weapons.bombs.CBU_105"] = 999
+stockpile_static["weapons.bombs.ROCKEYE"] = 999
+
+
+stockpile_static["weapons.missiles.AGM_65D"] = 999
+stockpile_static["weapons.missiles.AGM_65E"] = 999
+stockpile_static["weapons.missiles.AGM_65F"] = 999
+stockpile_static["weapons.missiles.AGM_65G"] = 999
+stockpile_static["weapons.missiles.AGM_65H"] = 999
+stockpile_static["weapons.missiles.AGM_65K"] = 999
+stockpile_static["weapons.missiles.AGM_65L"] = 999
+stockpile_static["weapons.missiles.AGM_84D"] = 999
+stockpile_static["weapons.missiles.AGM_84H"] = 999
+stockpile_static["weapons.missiles.AGM_84E"] = 999
+stockpile_static["weapons.missiles.AGM_88"] = 999
+stockpile_static["weapons.missiles.AGM_114K"] = 999 --Hellfire
+stockpile_static["weapons.missiles.AGM_122"] = 999 --sidearm
+stockpile_static["weapons.missiles.AGM_154A"] = 999
+stockpile_static["weapons.missiles.AGM_154C"] = 999
+stockpile_static["weapons.missiles.ADM_141A"] = 999 --F-14 TALD
+
+
+stockpile_static["weapons.missiles.AGR_20A"] = 999 -- HE APKWS
+stockpile_static["weapons.missiles.AGR_20_M282"] = 999 --MPP APKWS
+stockpile_static["weapons.nurs.HYDRA_70_MK5"] = 999 --HEAT Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M151"] = 999 --HE Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M156"] = 999 --White Phosphorus Rockets
+stockpile_static["weapons.nurs.HYDRA_70_M282"] = 999 -- APACHE MPP
+stockpile_static["weapons.nurs.HYDRA_70_M229"] = 999 -- APACHE HEDP
+
+stockpile_static["weapons.nurs.Zuni_127"] = 999
+
+--Viggen..
+stockpile_static["Rb 04E"] = 999
+stockpile_static["weapons.missiles.BK90_MJ1"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.BK90_MJ2"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.BK90_MJ1_MJ2"] = 999 --cluster bomblets
+stockpile_static["weapons.missiles.RB75"] = 999 --basically mav
+stockpile_static["weapons.missiles.RB75B"] = 999 --basically mav
+stockpile_static["weapons.missiles.RB75T"] = 999 --basically mav
+stockpile_static["Rb 15F"] = 999 --chonkers ASM
+stockpile_static["Rb 05A"] = 999
+stockpile_static["weapons.bombs.HEBOMB"] = 999
+stockpile_static["weapons.bombs.HEBOMBD"] = 999
+
+--mNEM JEEEFFF
+stockpile_static["weapons.missiles.GB-6"] = 999
+stockpile_static["weapons.missiles.GB-6-HE"] = 999
+stockpile_static["weapons.missiles.GB-6-SFW"] = 999
+stockpile_static["weapons.nurs.UG_90MM"] = 999
+stockpile_static["weapons.missiles.BRM-1_90MM"] = 999
+
+stockpile_static["weapons.missiles.C-701T"] = 999
+stockpile_static["weapons.missiles.C-701IR"] = 999
+stockpile_static["CM-802AKG"] = 999
+stockpile_static["weapons.missiles.C-802AK"] = 999
+stockpile_static["weapons.missiles.LD-10"] = 999
+stockpile_static["weapons.bombs.TYPE-200A"] = 999
+stockpile_static["weapons.missiles.LS-6-500"] = 999
+
+--Baguette HON HON
+stockpile_static["weapons.bombs.BAP_100"] = 999
+stockpile_static["weapons.bombs.BLG66_BELOUGA"] = 999
+
+
+--Russian Air to Ground
+
+stockpile_static["weapons.bombs.FAB_250"] = 999
+stockpile_static["weapons.bombs.FAB-250-M62"] = 999
+stockpile_static["weapons.bombs.FAB_500"] = 999
+stockpile_static["weapons.bombs.RBK_250"] = 999
+stockpile_static["weapons.bombs.RBK_250_275_AO_1SCH"] = 999 --RBK-250-275
+stockpile_static["weapons.bombs.RBK_500AO"] = 999 --RBK-500-255
+stockpile_static["weapons.bombs.RBK_500U"] = 999 --RBK 500
+stockpile_static["weapons.bombs.RBK_500U_OAB_2_SRT"] = 999 --RBK 500U
+stockpile_static["weapons.bombs.BetAB_500"] = 999
+stockpile_static["weapons.bombs.BetAB_500ShP"] = 999
+stockpile_static["weapons.bombs.BKF_AO2_5RT"] = 999 --HE/FRAG bomblet dispenser
+stockpile_static["weapons.bombs.BKF_PTAB_5KO"] = 999 --HEAT bomblet dispenser
+
+
+stockpile_static["weapons.nurs.C_5"] = 999 --S-5
+stockpile_static["weapons.nurs.C_8"] = 999 --S-8 HEAT
+stockpile_static["weapons.nurs.C_8OFP2"] = 999 --S-8 HE/FRAG
+stockpile_static["weapons.nurs.C_13"] = 999 --S-13
+stockpile_static["weapons.nurs.C_24"] = 999 --S-24B
+stockpile_static["weapons.nurs.C_25"] = 999 --S-25-OFM
+stockpile_static["weapons.nurs.S-25-O"] = 999 --S-25-O
+
+
+--mig21
+stockpile_static["weapons.nurs.S-24A"] = 999 --S-24A special mig version that slaps
+stockpile_static["weapons.nurs.S-24B"] = 999 --S-24B special mig version that slaps
+stockpile_static["weapons.bombs.RN-24"] = 999 --Small nuke
+stockpile_static["weapons.bombs.RN-28"] = 999 -- Big nuke
+stockpile_static["Kh-66_Grom"] = 999 --Air to ground missile, viability unknown
+stockpile_static["weapons.bombs.BL_755"] = 999 --some cluster bombs or something idk
+stockpile_static["weapons.bombs.FAB250M54TU"] = 999 --250kg bomb with drag chute
+
+--froggy
+stockpile_static["S_25L"] = 999 -- Laser Guided huge ass rocket
+stockpile_static["weapons.bombs.KAB_500Kr"] = 999 -- TV Guided Bomb
+stockpile_static["weapons.missiles.Vikhr_M"] = 999 --Vikhr (ATGM)
+stockpile_static["weapons.missiles.X_25ML"] = 999 --Kh-25ML (Soy Laser Guided)
+stockpile_static["weapons.missiles.Kh25MP_PRGS1VP"] = 999 --Kh-25MP (Super-Soy HARM)
+stockpile_static["weapons.missiles.X_25MP"] = 999 --Kh-25MPU (Soy HARM)
+stockpile_static["weapons.missiles.X_29T"] = 999 --Kh-29T (Chad TV Guided)
+stockpile_static["weapons.missiles.X_29L"] = 999 --Kh-29L (Chad Laser Guided)
+stockpile_static["weapons.missiles.X_58"] = 999 --Kh-58 (Chad HARM)
+
+--hind
+stockpile_static["weapons.missiles.AT_6"] = 999 --Shturm ATGM
+stockpile_static["weapons.missiles.Ataka_9M120"] = 999 --Ataka HEAT ATGM
+stockpile_static["weapons.missiles.Ataka_9M120F"] = 999 --Ataka HE ATGM
+stockpile_static["weapons.missiles.Ataka_9M220"] = 999 -- Ataka AA Frag ATGM
+
+--gazelle
+stockpile_static["weapons.nurs.SNEB68_EAP"] = 999 --gazelle rockets, why even
+stockpile_static["HOT3"] = 999 --ATGM
+stockpile_static["weapons.missiles.MBDA_Mistral"] = 999 -- AA Missile
+
+stockpile_static[""] = 999
